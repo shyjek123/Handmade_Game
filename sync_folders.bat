ff5842ce0d7e1b7f607cf39b6ed68a0b6ddc8f91
shyjek123 <sebby.hyjek@gmail.com>
2025-01-25 16:54:59 -0500

    updating push workflow syncfiles.bat

--- sync_folders.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET SOURCE_DIR="C:\Users\sebas\source\projects\Handmade_Game"
-SET TARGET_DIR="C:\Users\sebas\source\github\accounts\shyjek123\Handmade_Game"
-
-xcopy /E /I /H /Y "%SOURCE_DIR%\*" "%TARGET_DIR%\"
-
-echo Files copied from %SOURCE_DIR% to %TARGET_DIR%
-
