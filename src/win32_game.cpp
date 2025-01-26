8d4cc24a94a14bc77269da9d6e7bf4c492d27ea1
shyjek123 <sebby.hyjek@gmail.com>
2025-01-26 12:57:42 -0500

    day 3 roughly done still some updates needed

--- src/win32_game.cpp
+++ src/win32_game.cpp
@@ -1,5 +1,19 @@
 #include <windows.h>
+
+#define internal static
+#define local_persist static
+#define global static
+
+global bool Running;
+
+global BITMAPINFO bitmapInfo;
+global void *bitmapmem;
+global HBITMAP bmpHandle;
+global HDC BitmapDeviceContext;
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+internal void resizeDibSect(int width, int height);
+internal void updateWind(HDC dc, int x, int y, int width, int height);
 
 int CALLBACK WinMain(HINSTANCE inst, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
                      int nCmdShow) {
@@ -18,7 +32,8 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
         inst, NULL);
     if (windHandle) {
       MSG msg = {};
-      for (;;) {
+      Running = true;
+      while (Running) {
         if (GetMessage(&msg, NULL, 0, 0) > 0) {
           TranslateMessage(&msg);
           DispatchMessage(&msg);
@@ -39,13 +54,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
   LRESULT result = 0;
   switch (msg) {
   case WM_SIZE: {
+    RECT rect;
+    GetClientRect(hwnd, &rect);
+    int width = rect.right - rect.left;
+    int height = rect.bottom - rect.top;
+    resizeDibSect(width, height);
     OutputDebugString("WM_SIZE");
   } break;
   case WM_DESTROY: {
+    Running = false;
     OutputDebugString("WM_DESTROY");
   } break;
   case WM_CLOSE: {
-
+    Running = false;
     OutputDebugString("WM_CLOSE");
   } break;
   case WM_ACTIVATEAPP: {
@@ -56,14 +77,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     PAINTSTRUCT ps;
     HDC deviceContext = BeginPaint(hwnd, &ps);
 
-    DWORD color = WHITENESS;
-
     int upperLeftX = ps.rcPaint.left;
     int upperLeftY = ps.rcPaint.top;
     int width = ps.rcPaint.right - upperLeftX;
     int height = ps.rcPaint.bottom - upperLeftY;
-
-    PatBlt(deviceContext, upperLeftX, upperLeftY, width, height, color);
+    // TODO: update the window stretch the bits
+    updateWind(deviceContext, upperLeftX, upperLeftY, width, height);
     EndPaint(hwnd, &ps);
   } break;
   default: {
@@ -72,4 +91,30 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
   } break;
   }
   return result;
+}
+
+internal void resizeDibSect(int width, int height) {
+  if (bmpHandle) {
+    DeleteObject(bmpHandle);
+  }
+  if (!BitmapDeviceContext) {
+    BitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+  bitmapInfo.bmiHeader.biWidth = width;
+  bitmapInfo.bmiHeader.biHeight = height;
+  bitmapInfo.bmiHeader.biPlanes = 1;
+  bitmapInfo.bmiHeader.biPlanes = 32;
+  bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+  bmpHandle = CreateDIBSection(BitmapDeviceContext, &bitmapInfo, DIB_RGB_COLORS,
+                               &bitmapmem, NULL, NULL);
+}
+
+internal void updateWind(HDC dc, int x, int y, int width, int height) {
+  const void *img;
+  const BITMAPINFO *bitmapInfo;
+  StretchDIBits(dc, x, y, width, height, x, y, width, height, img, bitmapInfo,
+                DIB_RGB_COLORS, SRCCOPY);
 }
